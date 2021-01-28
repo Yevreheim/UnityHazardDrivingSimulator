6b737bc052064786949fb73c6783fd9322c6fefa
Yevreheim <Yehlemis@gmail.com>
2021-01-28 17:40:42 +1100

    Initial commit

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Threading;
+
+public class CameraMovement : MonoBehaviour
+{
+    //Variables
+    private int speedGo;
+    private float speedEnhancer = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speedGo = 0;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.GetKey(KeyCode.RightArrow)) || (Input.GetKey(KeyCode.D)))
+        {
+            transform.Translate(new Vector3(5 * Time.deltaTime, 0, 0));
+            //transform.Rotate(Vector3.up * speed * Time.deltaTime);
+        }
+        if ((Input.GetKey(KeyCode.LeftArrow)) || (Input.GetKey(KeyCode.A)))
+        {
+            transform.Translate(new Vector3(-5 * Time.deltaTime, 0, 0));
+            //transform.Rotate(-Vector3.up * speed * Time.deltaTime);
+        }
+        if ((Input.GetKey(KeyCode.DownArrow)) || (Input.GetKey(KeyCode.S)))
+        {
+            //transform.Translate(new Vector3(0, -3 * Time.deltaTime, 0));
+            //transform.Translate(new Vector3(0, 0, -3 * Time.deltaTime));
+            //transform.Rotate(Vector3.right * speed * Time.deltaTime);
+            if (speedEnhancer > 3){
+                speedEnhancer = speedEnhancer - 0.1f;
+            }
+        }
+        if ((Input.GetKey(KeyCode.UpArrow)) || (Input.GetKey(KeyCode.W)))
+        {
+            //transform.Translate(new Vector3(0, 3 * Time.deltaTime, 0));
+            speedEnhancer = speedEnhancer + 0.1f;
+            //transform.Translate(new Vector3(0, 0, 3 * Time.deltaTime));
+            //transform.Rotate(-Vector3.right * speed * Time.deltaTime);
+        }
+        if ((Input.GetKey(KeyCode.Space)))
+        {
+            switch (speedGo)
+            {
+                case 0:
+                    Debug.Log("Accelerating");
+                    Thread.Sleep(100);
+                    speedGo = 1;
+                    break;
+                case 1:
+                    Debug.Log("Breaking");
+                    Thread.Sleep(100);
+                    speedEnhancer = 3;
+                    speedGo = 0;
+                    break;
+            }
+        }
+        switch (speedGo)
+        {
+            case 0:
+                break;
+            case 1:
+                transform.Translate(new Vector3(0, 0, speedEnhancer * Time.deltaTime));
+                break;
+        }
+    }
+}
---
+++ Assets/Respawn.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Threading;
+using System.Linq;
+
+public class Respawn : MonoBehaviour
+{
+    //Variables
+    float x = 0;
+    float y = 0;
+    float z = 0;
+    float initialX;
+    float initialY;
+    float initialZ;
+    int[] Show = new int[2];
+    private GameObject[] CellArray;
+    private List<GameObject> Car = new List<GameObject>();
+    private System.Random Rand = new System.Random();
+    int CountTwo = 0;
+    private GameObject[] CarArray = new GameObject[4];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialX = transform.position.x;
+        initialY = transform.position.y;
+        initialZ = transform.position.z;
+        Show = ArrayRandomiser();
+        CellArray = CellCount(CellArray);
+
+        foreach (GameObject G in CellArray){
+            if (G.tag == "Car"){
+                //Convert to array instead
+                CarArray[CountTwo] = G;
+                CountTwo++;
+            }
+            if (G.name == "Plane1"){
+                G.GetComponent<Renderer>().material.SetColor("_Color",Color.blue);
+            }
+            else if (G.name == "Plane2"){
+                G.GetComponent<Renderer>().material.SetColor("_Color",Color.black);
+            }
+            else if (G.name == "Plane3"){
+                G.GetComponent<Renderer>().material.SetColor("_Color",Color.blue);
+            }
+            else if (G.name == "Plane4"){
+                G.GetComponent<Renderer>().material.SetColor("_Color",Color.black);
+            }
+        }
+        RandomAllocation();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = GameObject.FindWithTag("MainCamera").transform.position.x;
+        y = GameObject.FindWithTag("MainCamera").transform.position.y;
+        z = GameObject.FindWithTag("MainCamera").transform.position.z;
+
+        if (Input.GetKey(KeyCode.P))
+        {
+            Debug.Log("Postion of Camera X: " + x + " Y: " + y + " Z: " + z);
+            Debug.Log("Randomiser Value " + Show.First() + ","+ Show.Last());
+            Thread.Sleep(100);
+        }
+
+        //Respawning Process
+        if (z > transform.position.z + 5)
+        {
+            RandomAllocation();
+            transform.position = new Vector3(0,0,(transform.position.z + 50));
+            Show = ArrayRandomiser();
+            Debug.Log("New Position: " + transform.position.z);
+        }
+    }
+
+    private void RandomAllocation()
+    {
+        var randomInt = UnityEngine.Random.Range(0,2);
+        int[] PathRandom = new int[]{0,0,0,0};
+        int count = 0;
+        while (true){
+            foreach (int i in PathRandom){
+                if (i == 1){
+                    count++;
+                }   
+            }
+            if (count != 2) {
+                count = 0;
+                Rand = new System.Random();
+                for (int j = 0; j < PathRandom.Length; j++){
+                    if (randomInt == 0){
+                        PathRandom[j] = 0;
+                    }
+                    else if (randomInt == 1){
+                        PathRandom[j] = 1;
+                    }
+                    randomInt = UnityEngine.Random.Range(0,2);
+
+                }
+            }
+            else {
+                break;
+            }
+        }
+        for (int i = 0; i < PathRandom.Length; i++){
+            if (PathRandom[i] == 0){
+                CarArray[i].active = false;
+            }
+            else if (PathRandom[i] == 1){
+                CarArray[i].active = true;
+            }
+        }
+    }
+
+    private GameObject[] CellCount(GameObject[] G){
+        G = new GameObject[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            G[i] = transform.GetChild(i).gameObject;
+        }
+        return G;
+    }
+
+    private static int[] ArrayRandomiser()
+    {
+        int[] Test = new int[2];
+        System.Random Rd1 = new System.Random();
+        System.Random Rd2 = new System.Random();
+        int RandomNumber1 = Rd1.Next(0, 5);
+        int RandomNumber2 = Rd2.Next(0, 5);
+        Test[0] = RandomNumber1;
+        while (RandomNumber1 == RandomNumber2)
+        {
+            RandomNumber2 = Rd2.Next(1, 5);
+        }
+        Test[1] = RandomNumber2;
+        return Test;
+    }
+}
